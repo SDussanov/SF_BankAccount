19052a96eb55d9c3912a80cbd3291208ab18420e
SDussanov <dussanov.s@gmail.com>
2022-05-31 01:38:27 +0600

    1.0.2 доработка по этапу 2

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,4 +1,8 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.eclipse.jetty.http.DateParser.parseDate;
 
 public class Account {
 
@@ -9,6 +13,10 @@ public class Account {
 
     public Account(int id) {
         this.id = id;
+    }
+
+    public int getStatus() {
+        return this.status;
     }
 
     public int getBalance() {
@@ -27,6 +35,7 @@ public class Account {
             statement.close();
 
         } catch (SQLException e) {
+            System.out.println("Произошла ошибка с подключением к БД!");
             e.printStackTrace();
         }
         return amount;
@@ -40,17 +49,30 @@ public class Account {
             preparedStatement.setInt(1, (currentAmount + amount));
             preparedStatement.setInt(2, this.id);
 
-            int rows = preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
+
             System.out.println(String.format("Сумма обновлена с %s на %s", currentAmount, (currentAmount + amount)));
 
+            String sqlTransaction = "INSERT INTO OPERATIONS (ID_ACCOUNT, TYPE, AMOUNT, DATE) VALUES (?, 1, ?, ?)";
+            PreparedStatement psTransaction = connection.prepareStatement(sqlTransaction);
+            psTransaction.setInt(1, this.id);
+            psTransaction.setInt(2, amount);
+            psTransaction.setDate(3, new java.sql.Date(System.currentTimeMillis()));;
+
+            psTransaction.executeUpdate();
+            System.out.println("Добавил данные от транзакции в базу данных");
+
             connection.close();
+            psTransaction.close();
             preparedStatement.close();
             this.amount = (currentAmount + amount);
 
         } catch (SQLException e) {
+            System.out.println("Произошла ошибка с подключением к БД!");
             e.printStackTrace();
         }
     }
+
 
     public int takeMoney(int amount) {
         try (Connection connection = DriverManager.getConnection(URL)) {
@@ -62,10 +84,20 @@ public class Account {
                 preparedStatement.setInt(1, (currentAmount - amount));
                 preparedStatement.setInt(2, this.id);
 
-                int rows = preparedStatement.executeUpdate();
+                preparedStatement.executeUpdate();
                 System.out.println(String.format("Сумма обновлена с %s на %s", currentAmount, (currentAmount - amount)));
 
+                String sqlTransaction = "INSERT INTO OPERATIONS (ID_ACCOUNT, TYPE, AMOUNT, DATE) VALUES (?, 2, ?, ?)";
+                PreparedStatement psTransaction = connection.prepareStatement(sqlTransaction);
+                psTransaction.setInt(1, this.id);
+                psTransaction.setInt(2, amount);
+                psTransaction.setDate(3, new java.sql.Date(System.currentTimeMillis()));;
+
+                psTransaction.executeUpdate();
+                System.out.println("Добавил данные о транзакции в базу данных");
+
                 connection.close();
+                psTransaction.close();
                 preparedStatement.close();
                 this.amount = (currentAmount - amount);
                 this.status = 1;
@@ -76,9 +108,50 @@ public class Account {
 
 
         } catch (SQLException e) {
+            System.out.println("Произошла ошибка с подключением к БД!");
             e.printStackTrace();
         }
         return this.status;
+    }
+
+    public List<String> getOperationList(Date firstDate){
+
+        List<String> data = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(URL)) {
+
+            String sql = "SELECT * FROM OPERATIONS WHERE ID_ACCOUNT = ? AND DATE = ?";
+
+//Date startDate = parseDate(firstDate);
+
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+                preparedStatement.setInt(1, this.id);
+                preparedStatement.setDate(2, firstDate);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                data.add("User: id=" + resultSet.getInt("id_account") +
+                        " type=" + resultSet.getString("type") +
+                        " amount=" + resultSet.getString("amount") +
+                        " date=" + resultSet.getString("date"));
+
+                System.out.println("User: id=" + resultSet.getInt("id_account") +
+                        " type=" + resultSet.getString("type") +
+                        " amount=" + resultSet.getString("amount") +
+                        " date=" + resultSet.getString("date"));
+            }
+
+            connection.close();
+            resultSet.close();
+            preparedStatement.close();
+            return data;
+        } catch (SQLException e) {
+            System.out.println("Произошла ошибка с подключением к БД!");
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,32 @@
 import com.google.gson.Gson;
 
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
+import java.util.List;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+
         get("/getBalance/:id", ((request, response) -> {
+            response.type("application/json");
             int index = Integer.parseInt(request.params("id"));
             Account account = new Account(index);
             return new Gson().toJson(account.getBalance());
         }));
 
-        post("/putMoney/:id/:amount", ((request, response) -> {
+        get("/getOperationList/:id/:date", ((request, response) -> {
+            int index = Integer.parseInt(request.params("id"));
+            Account account = new Account(index);
+            List<String> test = account.getOperationList(new java.sql.Date(System.currentTimeMillis()));
+            //Date startDate = (Date) new SimpleDateFormat("dd-MM-yyyy").parse(request.params("date"));
             response.type("application/json");
+            return new Gson().toJson(test);
+        }));
 
+        post("/putMoney/:id/:amount", ((request, response) -> {
             Account account = new Account(Integer.parseInt(request.params("id")));
             account.putMoney(Integer.parseInt((request.params("amount"))));
             return ("Добавлено " + request.params("amount") + "$ теперь на счету " + account.getBalance() + "$");
@@ -21,13 +35,12 @@ public class Main {
         post("/takeMoney/:id/:amount", ((request, response) -> {
             Account account = new Account(Integer.parseInt(request.params("id")));
             account.takeMoney(Integer.parseInt((request.params("amount"))));
-            int status = account.takeMoney(Integer.parseInt((request.params("amount"))));
+            int status = account.getStatus();
             if (status == 1) {
                 return ("Списано " + request.params("amount") + "$ теперь на счету " + account.getBalance() + "$");
             } else {
                 return ("Не хватает баланса, на вашем счету " + account.getBalance() + "$");
             }
-
         }));
 
     }
